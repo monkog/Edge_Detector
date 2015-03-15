cd4b0afcf06db2bbc564ccbc5c543be1147d2723
Monika Kogut <mkmonikakogut@gmail.com>
2015-03-15 17:25:19 +0100

    Added circles detection (not working)

--- ParametrizedEdgeDetector/main.cpp
+++ ParametrizedEdgeDetector/main.cpp
@@ -11,11 +11,54 @@ Mat src_blur;
 int lowT = 50;
 int sobol = 3;
 
+void calculateLines(Mat result)
+{
+	vector<Vec2f> lines;
+	Mat src_lines;
+	HoughLines(result, lines, 1, CV_PI / 180, 200);
+	cvtColor(result, src_lines, CV_GRAY2BGR);
+
+	for (size_t i = 0; i < lines.size(); i++)
+	{
+		float rho = lines[i][0], theta = lines[i][1];
+		Point pt1, pt2;
+		double a = cos(theta), b = sin(theta);
+		double x0 = a*rho, y0 = b*rho;
+		pt1.x = cvRound(x0 + 1000 * (-b));
+		pt1.y = cvRound(y0 + 1000 * (a));
+		pt2.x = cvRound(x0 - 1000 * (-b));
+		pt2.y = cvRound(y0 - 1000 * (a));
+		line(src_lines, pt1, pt2, Scalar(0, 0, 255), 1, CV_AA);
+	}
+	imshow("detected lines", src_lines);
+}
+
+void calculateCircles(Mat result)
+{
+	vector<Vec3f> circles; 
+	Mat src_circles;
+	cout << "ROWS:" << result.rows << "/ROWS";
+	HoughCircles(result, circles, CV_HOUGH_GRADIENT, 1, result.rows / 8);
+	cvtColor(result, src_circles, CV_GRAY2BGR);
+
+	for (size_t i = 0; i < circles.size(); i++) 
+	{ 
+		Point center(cvRound(circles[i][0]), cvRound(circles[i][1])); 
+		int radius = cvRound(circles[i][2]); 
+		circle(src_circles, center, 3, Scalar(0, 255, 0), -1, 8, 0);
+		circle(src_circles, center, radius, Scalar(0, 0, 255), 3, 8, 0);
+	} 
+	imshow("detected circles", src_circles);
+}
+
 void calculateResultMatrix(int treshold, int sobol)
 {
 	Mat result;
+	cout << "\n" << treshold << "\t" << sobol;
 	Canny(src_blur, result, treshold, 3 * treshold, sobol, true);
 	imshow("Parametrized edge detector", result);
+	calculateLines(result);
+	calculateCircles(result);
 }
 
 void changeTreshold(int newValue, void * object)
@@ -29,7 +72,7 @@ void changeSobol(int newValue, void * object)
 	switch (newValue)
 	{
 	case 0:
-		sobol = -1;
+		sobol = CV_SCHARR;
 		break;
 	default:
 		sobol = newValue * 2 + 1;
@@ -50,13 +93,17 @@ int main(int argc, char* argv[])
 	Mat src_gray, result;
 	cvtColor(src, src_gray, CV_BGR2GRAY);
 	GaussianBlur(src, src_blur, Size(5, 5), 0);
-	Canny(src_blur, result, lowT, 3 * lowT, 3, true);
+	Canny(src_blur, result, 150, 3 * 150, 3, true);
 
 	namedWindow("Parametrized edge detector", WINDOW_AUTOSIZE);
-	createTrackbar("sobol", "Parametrized edge detector", &sobol, 4, &changeSobol);
+	createTrackbar("sobol", "Parametrized edge detector", &sobol, 3, &changeSobol);
 	createTrackbar("low treshold", "Parametrized edge detector", &lowT, 100, &changeTreshold);
 
 	imshow("Parametrized edge detector", result);
+	namedWindow("detected lines", CV_WINDOW_AUTOSIZE);
+	namedWindow("detected circles", CV_WINDOW_AUTOSIZE);
+	calculateLines(result);
+	calculateCircles(result);
 
 	waitKey(0); // Wait for a keystroke in the window
 	return 0;
